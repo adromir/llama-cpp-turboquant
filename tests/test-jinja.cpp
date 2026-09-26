cc74bafdb73dd1b5a52775b1205f0c5f93eeff93
Sigbjørn Skjæret <sigbjorn.skjaeret@huggingface.co>
2026-09-26 08:56:48 +0200

    jinja : support noncall test statements with arg (#29443)
    * support noncall test statements with arg
    
    * add tests

--- tests/test-jinja.cpp
+++ tests/test-jinja.cpp
@@ -1087,7 +1087,7 @@ static void test_tests(testing & t) {
     );
 
     test_template(t, "is not equalto",
-        "{{ 'yes' if 3 is not equalto(4) }}",
+        "{{ 'yes' if 3 is not equalto 4 }}",
         json::object(),
         "yes"
     );
@@ -1099,7 +1099,7 @@ static void test_tests(testing & t) {
     );
 
     test_template(t, "is gt",
-        "{{ 'yes' if 3 is gt(2) }}",
+        "{{ 'yes' if 3 is gt 2 }}",
         json::object(),
         "yes"
     );
@@ -1111,7 +1111,7 @@ static void test_tests(testing & t) {
     );
 
     test_template(t, "is lt",
-        "{{ 'yes' if 2 is lt(3) }}",
+        "{{ 'yes' if 2 is lt 3 }}",
         json::object(),
         "yes"
     );
@@ -1125,6 +1125,12 @@ static void test_tests(testing & t) {
     test_template(t, "is ne",
         "{{ 'yes' if 2 is ne(3) }}",
         json::object(),
+        "yes"
+    );
+
+    test_template(t, "is lt and gt",
+        "{{ 'yes' if x is lt 3 and x is gt 1 }}",
+        {{"x", 2}},
         "yes"
     );
 
@@ -1179,6 +1185,12 @@ static void test_tests(testing & t) {
     test_template(t, "is integer",
         "{{ 'yes' if x is integer }}",
         {{"x", 1}},
+        "yes"
+    );
+
+    test_template(t, "is integer or float",
+        "{{ 'yes' if x.y is integer or x.y is float else 'no' }}",
+        {{"x", {{"y", 1.1}}}},
         "yes"
     );
 
